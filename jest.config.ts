0d0e6853d1a65c75e44327fa39c5e7cb81cd6342
tuandung1304 <tuandung13401@gmail.com>
2025-11-16 23:48:48 +0700

    Create Nest GraphQL App

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  passWithNoTests: true,
+};
+
+export default config;
